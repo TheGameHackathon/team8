c89b5254a80e02ed3d43fb2375446ee34c5ae70f
Dmitry Shchapin <diman199806@gmail.com>
2019-02-22 23:38:22 +0500

    x

--- src/Startup.cs
+++ src/Startup.cs
@@ -32,7 +32,7 @@ namespace thegame
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
