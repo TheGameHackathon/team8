7a156ead176420c48875bb9c721d84b18a8c4484
Alexandr Debelyi <debelyy@yandex.ru>
2019-02-23 11:40:04 +0500

    some things

--- src/Controllers/GameController.cs
+++ src/Controllers/GameController.cs
@@ -5,10 +5,33 @@ namespace thegame.Controllers
     [Route("api/game")]
     public class GameController : Controller
     {
+
+        public int[,] Map {get; set;} = new int[4,8];
+
         [HttpGet("score")]
         public IActionResult Score()
         {
             return Ok(50);
         }
+
+        [HttpGet("start")]
+        public IActionResult Start()
+        {
+            Map = new int[,] {
+                {1, 2, 3, 4, 1, 2, 3, 4},
+                {5, 6, 7, 8, 5, 6, 7, 8},
+                {9, 10, 11, 12, 9, 10, 11, 12},
+                {13, 14, 15, 0, 13, 14, 15, 0},      
+            };
+            return Ok(Map);
+        }
+
+        [HttpPost("turn")]
+        public IActionResult Turn([FromBody] TurnDTO turn)
+        {
+            return Ok();
+        }
     }
+
+    
 }
