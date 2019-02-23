c37879a0bbe261b32c59ae841a862b00d75fdba9
Leoltron <leoole5@gmail.com>
2019-02-23 12:06:07 +0500

    I'm alive!

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -6,19 +6,28 @@ import Background from './images/card-joker.png';
 export default class Field extends React.Component {
     constructor({cardState, rows, cardsInRow}) {
         super();
-        this.cardState = cardState;
-        this.rowsCount = rows;
-        this.cardsInRow = cardsInRow;
+        this.state = {
+            cardState: cardState,
+            rowsCount: rows,
+            cardsInRow: cardsInRow
+        };
+    }
+
+    switchCard(cardId) {
+        console.log(cardId);
+        const state = this.state.cardState;
+        state[cardId] = 1;
+        this.setState({cardState: state})
     }
 
     renderCard(cardId) {
-        const typeId = this.cardState[cardId];
+        const typeId = this.state.cardState[cardId];
         let cardDivId = "card_" + cardId;
         return (
-            <td className={styles.cell}>
-                <div
-                    className={typeId === -1 ? styles.card : classNames(styles.card, styles.cardFlipped)}
-                    id={cardDivId}>
+            <td className={styles.cell} key={cardDivId}>
+                <div onClick={typeId === -1 && (() => this.switchCard(cardId))}
+                     className={typeId === -1 ? styles.card : classNames(styles.card, styles.cardFlipped)}
+                     id={cardDivId}>
                     <div className={classNames(styles.cardSide, styles.cardBack)}/>
                     <div className={classNames(styles.cardSide, styles.cardFace)}
                          style={{backgroundImage: `url(${Background})`}}/>
@@ -29,11 +38,11 @@ export default class Field extends React.Component {
 
     renderRow(rowId) {
         const cards = [];
-        for (let i = 0; i < this.cardsInRow; i++) {
-            cards.push(this.renderCard(rowId * this.cardsInRow + i));
+        for (let i = 0; i < this.state.cardsInRow; i++) {
+            cards.push(this.renderCard(rowId * this.state.cardsInRow + i));
         }
         return (
-            <tr className={styles.row}>
+            <tr className={styles.row} key={rowId}>
                 {cards}
             </tr>
         );
@@ -41,12 +50,14 @@ export default class Field extends React.Component {
 
     render() {
         const rows = [];
-        for (let i = 0; i < this.rowsCount; i++) {
+        for (let i = 0; i < this.state.rowsCount; i++) {
             rows.push(this.renderRow(i));
         }
         return (
             <table className={styles.field}>
+                <tbody>
                 {rows}
+                </tbody>
             </table>
         );
     }
