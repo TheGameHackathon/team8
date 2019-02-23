c0ce7ddf569557eed8636978196f280d3118c74d
Dmitry Shchapin <diman199806@gmail.com>
2019-02-23 15:20:24 +0500

    GameState repository

---
+++ src/Domain/InMemoryGameRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace thegame.Domain
+{
+    public class InMemoryGameRepository :IGameStateRepository
+    {
+        private readonly Dictionary<Guid, GameState> gameStates;
+
+        public InMemoryGameRepository()
+        {
+            this.gameStates = new Dictionary<Guid, GameState>();
+        }
+
+
+        public void Insert(GameState gameState)
+        {
+            gameState.Id = gameState.Id;
+            gameStates[gameState.Id] = gameState;
+        }
+
+        public void Update(GameState gameState)
+        {
+            if (gameStates.ContainsKey(gameState.Id))
+            {
+                gameStates[gameState.Id] = gameState;
+            }
+        }
+
+        public GameState FindById(Guid id)
+        {
+            if (gameStates.ContainsKey(id))
+            {
+                return gameStates[id];
+            }
+
+            return null;
+        }
+    }
+}
